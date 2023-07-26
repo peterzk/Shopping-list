a39ec8c94d26ad0c19ba99d30c7ed336a239189d
peterzk <kiragu303@gmail.com>
2023-07-26 22:50:16 +0300

    Added item clearing & UI clearing

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 
 function addItem(e){
@@ -22,7 +25,10 @@ function addItem(e){
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = '';
 
@@ -48,6 +54,47 @@ function createIcon(classes){
 
 }
 
+// Function for removing item
+
+function removeItem(e){
+   if(e.target.parentElement.classList.contains('remove-item')){
+    if (confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove()
+
+      checkUI();
+    }
+   }
+}
+
+// Function for clearing all the items
+
+function clearItems(){
+  while (itemList.firstChild){
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  checkUI();
+}
+
+// Function to check whether there are list items in UI
+function checkUI(){
+
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0){
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else{
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+};
+
+
 // Event Listeners
 
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+
+
+checkUI();
