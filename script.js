e024dcc7692da8968dbad3ffb659201811483425
peterzk <kiragu303@gmail.com>
2023-07-27 23:59:13 +0300

    Adding to local storage

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
 
 
+function displayItems(){
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach(item =>addItemToDOM(item));
 
-function addItem(e){
+  checkUI();
+}
+
+function onAddItemSubmit(e){
   e.preventDefault()
 
   const newItem = itemInput.value;
@@ -17,22 +23,31 @@ function addItem(e){
     return;
   }
 
-  // create list item
+  // Create item DOM element
+  addItemToDOM(newItem);
 
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
-
-  const button = createButton('remove-item btn-link text-red');
-  li.appendChild(button);
-
-  // Add li to the DOM
-  itemList.appendChild(li);
+  // Add item to local storage
+  addItemToStorage(newItem);
 
   checkUI();
 
   itemInput.value = '';
 
 }
+
+function addItemToDOM(item){
+  // create list item
+
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+}
+
 
 
 // Function for creating the button and icon
@@ -54,6 +69,32 @@ function createIcon(classes){
 
 }
 
+
+// function to add to local storage
+
+function addItemToStorage(item){
+  const itemsFromStorage = getItemsFromStorage();
+
+  // add new item to array
+  itemsFromStorage.push(item);
+
+  // covert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+};
+
+// getting items from storage
+
+function getItemsFromStorage(){
+  let itemsFromStorage;
+
+  if (localStorage.getItem('items')===null){
+    itemsFromStorage = [];
+  } else{
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+
+  }
+  return itemsFromStorage;
+};
 // Function for removing item
 
 function removeItem(e){
@@ -76,6 +117,26 @@ function clearItems(){
   checkUI();
 }
 
+// function for the filter input
+
+function filterItems(e){
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach(item => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1){
+      item.style.display = 'flex';
+    } else{
+      item.style.display = 'none';
+    }
+    
+  });
+
+  
+}
+
 // Function to check whether there are list items in UI
 function checkUI(){
 
@@ -89,12 +150,18 @@ function checkUI(){
   }
 };
 
+// Initialize app
 
-// Event Listeners
+function init(){
+  // Event Listeners
 
-itemForm.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
-clearBtn.addEventListener('click', clearItems);
+  itemForm.addEventListener('submit', onAddItemSubmit);
+  itemList.addEventListener('click', removeItem);
+  clearBtn.addEventListener('click', clearItems);
+  itemFilter.addEventListener('input', filterItems);
+  document.addEventListener('DOMContentLoaded', displayItems);
 
+  checkUI();
+}
 
-checkUI();
+init();
